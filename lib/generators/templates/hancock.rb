6acc71f5fd60afdf8c8828f6e426541dcc2ed660
kbokhonko <kolya.bokhonko@gmail.com>
2014-04-01 12:28:47 +0300

    add configs

--- lib/generators/templates/hancock.rb
+++ lib/generators/templates/hancock.rb
@@ -5,16 +5,16 @@ Hancock.configure do |c|
    c.password       = '12345'
    c.integrator_key = 'YAY-9ED8E711-C191-4265-AFCC-253F6241207A'
    c.account_id     = '999999'
-   #c.endpoint       = 'https://www.docusign.net/restapi'
-   #c.api_version    = 'v2'
+   c.endpoint       = 'https://www.docusign.net/restapi'
+   c.api_version    = 'v2'
 
-  c.event_notification = {
-    :logging_enabled => true,
-    :uri => 'http://callback.com',
-    :include_document => true,
-  }
-  c.email_template = {
-    :subject => 'sign me',
-    :blurb => 'whatever '
-  }
+  # c.event_notification = {
+  #   :logging_enabled => true,
+  #   :uri => 'http://callback.com',
+  #   :include_document => true,
+  # }
+  # c.email_template = {
+  #   :subject => 'sign me',
+  #   :blurb => 'whatever '
+  # }
 end
--- lib/hancock.rb
+++ lib/hancock.rb
@@ -1,5 +1,23 @@
 require "hancock/version"
 
+require_relative 'hancock/configuration'
+
 module Hancock
-  # Your code goes here...
+  extend Configuration
+	class Envelope 
+	  
+
+		#
+		#for test connection
+		#
+	  def build_uri(url)
+	    URI("#{Hancock.endpoint}/#{Hancock.api_version}#{url}")
+	  end
+
+	  def get_login_information(options={})
+	    uri = build_uri('/login_information')
+	    request = Net::HTTP.get(uri)
+	    # p request
+	  end
+	end
 end
