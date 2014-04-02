fe13672f8f9d492d1c121085d0e5ce843b2f90ea
kbokhonko <kolya.bokhonko@gmail.com>
2014-04-02 14:26:09 +0300

    initial structure of document and envelope classes

--- lib/hancock/document.rb
+++ lib/hancock/document.rb
@@ -1,4 +1,15 @@
 module Hancock
   class Document
+  	ATTRIBUTES = [:file, :data, :name, :extension, :identifier]
+    
+    ATTRIBUTES.each do |attr|
+      self.send(:attr_accessor, attr)
+    end
+
+    def initialize(attributes = {})
+      ATTRIBUTES.each do |attr|
+        self.send("#{attr}=", attributes[attr])
+      end
+    end
   end
 end
