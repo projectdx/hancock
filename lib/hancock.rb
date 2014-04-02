e9158ff9f7e79e1ff8741178898b26d13e7e3ea8
yasinishyn <balabanpetr@MacBook-Air-Petr.local>
2014-04-02 09:11:03 +0300

    base architecture (folders, files, rspec setup)

--- lib/hancock.rb
+++ lib/hancock.rb
@@ -1,36 +1,14 @@
-require "hancock/version"
+require 'hancock/version'
+require 'hancock/envelope'
+require 'hancock/document'
+require 'hancock/anchored_tab'
+require 'hancock/tab'
+require 'hancock/envelope_status'
+require 'hancock/recepient_status'
+require 'nokogiri'
 
 require_relative 'hancock/configuration'
 
 module Hancock
   extend Configuration
-	class Envelope 
-	  
-
-		#
-		#for test connection
-		#
-	  def build_uri(url)
-	    URI.parse("#{Hancock.endpoint}/#{Hancock.api_version}#{url}")
-	  end
-
-	  def get_login_information(options={})
-	    uri = build_uri('/login_information')
-	    
-	    headers = {
-          'X-DocuSign-Authentication' => {
-            'Username' => Hancock.username,
-            'Password' => Hancock.password,
-            'IntegratorKey' => Hancock.integrator_key
-          }.to_json
-        }
-
-	    request = Net::HTTP::Get.new(uri.request_uri, headers)
-	   	http = Net::HTTP.new(uri.host)		
-			response = http.request(request)
-
-	    response
-	    
-	  end
-	end
 end
---
+++ lib/hancock/envelope_status.rb
@@ -0,0 +1,6 @@
+module Hancock
+  class EnvelopeStatus
+    def initialize(xml)
+    end
+  end
+end
