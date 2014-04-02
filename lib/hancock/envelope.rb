d1b67288bf5e854fb8522d7b59208728620d0c5c
kbokhonko <kolya.bokhonko@gmail.com>
2014-04-02 18:39:28 +0300

    refactor class envelope, add new actions documents, recipients...

--- lib/hancock/envelope.rb
+++ lib/hancock/envelope.rb
@@ -12,76 +12,110 @@ module Hancock
           'Username' => Hancock.username,
           'Password' => Hancock.password,
           'IntegratorKey' => Hancock.integrator_key
-        }.to_json
+        }
       }
     end
 
-    def add_document document 
+    def add_document(document) 
       @documents << { documentId: document.identifier, name: document.name }
     end
 
-    def add_signature_request recepient, document, tabs 
-        doc_signer = {
-          email: recepient[:email],
-          name: recepient[:name],
-          recipientId:  recepient[:id]
-        }
-
-        doc_signer[:tabs] = {
-          #here we can add more types
-          initialHereTabs:      get_tabs( tabs, "initial_here", document.identifier),
-          signHereTabs:         get_tabs( tabs, "sign_here" , document.identifier),
-        }
-
-        @recipients[:signers] << doc_signer
-      end
-    end
-
-    def send!
-      post_body = {
-          emailBlurb:   "emailBlurb",
-          emailSubject: "emailSubject",
-          status: "sent",
-          documents: @documents,
-          recipients: @recipients,
-        }.to_json
-
-      uri = build_uri("/accounts/#{Hancock.account_id}/envelopes")
-      http = initialize_http(uri)
-
-      headers = {
-        'Content-Type'              => "multipart/form-data",
-        'Accept'                    => 'application/json',
-        'Content-Length'            => "#{@post_body.length}"
+    def add_signature_request(recepient, document, tabs) 
+      doc_signer = {
+        email: recepient[:email],
+        name: recepient[:name],
+        recipientId:  recepient[:id]
       }
-      headers.merge!(@docusign_headers)
 
-      request = Net::HTTP::Post.new(uri.request_uri, headers)
-      request.body = post_body
-      response = http.request(request)
-      response.body
+      doc_signer[:tabs] = {
+        #here we can add more types
+        initialHereTabs:      get_tabs( tabs, "initial_here", document.identifier),
+        signHereTabs:         get_tabs( tabs, "sign_here" , document.identifier),
+      }
+
+      @recipients[:signers] << doc_signer
     end
 
-    def build_uri(url)
-      URI.parse("#{Hancock.endpoint}/#{Hancock.api_version}#{url}")
-    end
-
-    #
-    #for testing connection
-    #
-    def get_login_information(options={})
-      uri = build_uri('/login_information')
+    #################need refactor
+    def send!
+      uri = build_uri("/accounts/#{Hancock.account_id}/envelopes")
+      body_post = get_post_body("sent")     
       
-      http = initialize_http(uri)
+      content_headers = { 'Content-Type' => 'multipart/form-data', 'Content-Length' => "#{body_post.length}" }
 
-      request = Net::HTTP::Get.new(uri.request_uri, @docusign_headers)
-      response = http.request(request)
-      response
+      post_request(uri, body_post, get_headers(content_headers))
+    end
+
+    def save
+      uri = build_uri("/accounts/#{Hancock.account_id}/envelopes")
+      body_post = get_post_body("created")     
+
+      content_headers = { 'Content-Type' => 'multipart/form-data', 'Content-Length' => "#{body_post.length}" }
+
+      post_request(uri, body_post, get_headers(content_headers))
+    end
+    #################################
+
+    def documents
+      uri = build_uri("/accounts/#{Hancock.account_id}/envelopes/#{envelope_id}/documents")
+      content_headers = { 'Content-Type' => 'application/json' }
+
+      get_request(uri, headers)
+    end
+
+    def recipients
+      uri = build_uri("/accounts/#{Hancock.account_id}/envelopes/#{envelope_id}/recipients")
+      content_headers = { 'Content-Type' => 'application/json' }
+
+      get_request(uri, headers)
+    end
+
+    def status
+      #
     end
 
     private
+      def post_request(uri, body_post, headers)
+        http = initialize_http(uri)
 
-      def initialize_http uri
+        request = Net::HTTP::Post.new(uri.request_uri, headers)
+        request.body = body_post
+        response = http.request(request)
+        response
+      end
+
+      def get_request(uri, headers)
+        http = initialize_http(uri)
+
+        rrequest = Net::HTTP::Get.new(uri.request_uri, headers)
+        response = http.request(request)
+        response
+      end
+
+      def build_uri(url)
+        URI.parse("#{Hancock.endpoint}/#{Hancock.api_version}#{url}")
+      end
+
+      def get_post_body(status)
+        { 
+          emailBlurb:   "emailBlurb",
+          emailSubject: "emailSubject",
+          status: "#{status}",
+          documents: @documents,
+          recipients: @recipients,
+        }
+      end
+
+      def get_headers(user_defined_headers={})
+        default = {
+          'Accept' => 'json' 
+        }
+
+        default.merge!(user_defined_headers) if user_defined_headers
+        @docusign_headers.merge(default)
+      end
+
+      def initialize_http(uri)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -90,7 +124,6 @@ module Hancock
 
       def get_tabs(tabs, type, document_id)
         tab_array = []
-
         tabs_by_type = tabs.select{ |h| h.values_at('type', type) }
 
         tabs_by_type.map do |tab|
@@ -99,7 +132,7 @@ module Hancock
           if tab.is_a? Hancock::AnchoredTab
             tab_hash[:anchorString] = tab[:anchor_string]
             tab_hash[:anchorXOffset] = tab[:offset][0] || '0'
-            tab_hash[:anchorYOffset]  = tab[:offset][1 || '0'
+            tab_hash[:anchorYOffset]  = tab[:offset][1] || '0'
           else
             tab_hash[:xPosition]  = tab[:coordinates][0] || '0'
             tab_hash[:yPosition]  = tab[:coordinates][1] || '0'          
@@ -107,7 +140,6 @@ module Hancock
 
           tab_hash[:documentId] = document_id || '0'
           tab_hash[:scaleValue] = tab[:scaleValue] || 1
-
 
           tab_hash[:name]       = tab[:name] if tab[:name]
           tab_hash[:tabLabel]   = tab[:label] || 'Signature 1'
