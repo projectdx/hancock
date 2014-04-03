1a26c8ef912f42bbee2088ee26d857453bf3cce6
kbokhonko <kolya.bokhonko@gmail.com>
2014-04-03 17:30:09 +0300

    change generating post body, refactor

--- lib/hancock/envelope.rb
+++ lib/hancock/envelope.rb
@@ -1,6 +1,12 @@
 module Hancock
   class Envelope
     
+    BOUNDARY = 'AAA'
+
+    def self.find
+      ###
+    end
+
     def initialize
       @documents = []
       @recipients = {
@@ -12,19 +18,22 @@ module Hancock
           'Username' => Hancock.username,
           'Password' => Hancock.password,
           'IntegratorKey' => Hancock.integrator_key
-        }
+        }.to_json
       }
+
+      @files_array = []
     end
 
     def add_document(document) 
       @documents << { documentId: document.identifier, name: document.name }
+      @files_array << document
     end
 
     def add_signature_request(recepient, document, tabs) 
       doc_signer = {
-        email: recepient[:email],
-        name: recepient[:name],
-        recipientId:  recepient[:id]
+        email: recepient.email,
+        name: recepient.name,
+        recipientId:  recepient.identifier
       }
 
       doc_signer[:tabs] = {
@@ -36,29 +45,47 @@ module Hancock
       @recipients[:signers] << doc_signer
     end
 
-    #################need refactor
+    #################need refactor    
+    def form_post_body status     
+      post_body = ''
+      post_body << "\r\n"
+      post_body << "--#{BOUNDARY}\r\n"
+      post_body << "Content-Type: application/json\r\n"
+      post_body << "Content-Disposition: form-data\r\n"
+      post_body << "\r\n"
+      post_body << get_post_params(status).to_json
+      post_body << "\r\n"
+      post_body << "--#{BOUNDARY}\r\n"
+      
+
+      @files_array.each do |f|
+        post_body << "Content-Type: application/pdf\r\n"
+        post_body << "Content-Disposition: file; filename=#{f.name}; documentid=#{f.identifier}\r\n"
+        post_body << "\r\n"
+        post_body << IO.read(f.file) 
+        post_body << "\r\n"
+      end
+
+      post_body << "\r\n"
+      post_body << "--#{BOUNDARY}--\r\n"
+    end
+
     def send!
       uri = build_uri("/accounts/#{Hancock.account_id}/envelopes")
-      body_post = get_post_body("sent")     
-      
       content_headers = { 
-        'Content-Type' => 'multipart/form-data', 
-        'Content-Length' => "#{body_post.length}" 
+        'Content-Type' => "multipart/form-data, boundary=#{BOUNDARY}" 
       }
 
-      post_request(uri, body_post, get_headers(content_headers))
+      post_request(uri, form_post_body("sent"), get_headers(content_headers))
     end
 
     def save
       uri = build_uri("/accounts/#{Hancock.account_id}/envelopes")
-      body_post = get_post_body("created")     
-
       content_headers = { 
-        'Content-Type' => 'multipart/form-data', 
-        'Content-Length' => "#{body_post.length}" 
+        'Content-Type' => "multipart/form-data, boundary=#{BOUNDARY}" 
       }
 
-      post_request(uri, body_post, get_headers(content_headers))
+      post_request(uri, form_post_body("created"), get_headers(content_headers))
     end
     #################################
 
@@ -80,6 +107,20 @@ module Hancock
       #
     end
 
+    #
+    #for test request from console
+    #
+    # def self.test
+    #   envelope = Hancock::Envelope.new
+    #   doc1 = File.open("test.pdf")
+    #   document1 = Hancock::Document.new(file: doc1, name: "test", extension: "pdf", identifier: "123")
+    #   recipient1 = Hancock::Recipient.new(name: "Owner", email: "kolya.bokhonko@gmail.com", routing_order: 1)
+    #   envelope.add_document(document1)
+    #   tab1 = Hancock::Tab.new(type: "sign_here", label: "Vas", coordinates: [2, 100], page_number: 1)
+    #   envelope.add_signature_request(recipient1, document1, [tab1])
+    #   envelope.save
+    # end
+
     private
       def post_request(uri, body_post, headers)
         http = initialize_http(uri)
@@ -100,7 +141,7 @@ module Hancock
         URI.parse("#{Hancock.endpoint}/#{Hancock.api_version}#{url}")
       end
 
-      def get_post_body(status)
+      def get_post_params(status)
         { 
           emailBlurb:   "emailBlurb",
           emailSubject: "emailSubject",
@@ -133,30 +174,25 @@ module Hancock
       end
 
       def tabs_by_type(tabs, type)
-        tabs.select{ |h| h.values_at('type', type) }
+        tabs.select{ |h| h.type == type }
       end
 
       def generate_tab(tab, document_id)
         tab_hash = {}
 
         if tab.is_a? Hancock::AnchoredTab
-          tab_hash[:anchorString]  = tab[:anchor_string]
-          tab_hash[:anchorXOffset] = tab[:offset][0] || '0'
-          tab_hash[:anchorYOffset] = tab[:offset][1] || '0'
+          tab_hash[:anchorString]  = tab.anchor_text || 'Signature 1'
+          tab_hash[:anchorXOffset] = tab.offset[0] || '0'
+          tab_hash[:anchorYOffset] = tab.offset[1] || '0'
+          tab_hash[:IgnoreIfNotPresent] = 1
         else
-          tab_hash[:xPosition]  = tab[:coordinates][0] || '0'
-          tab_hash[:yPosition]  = tab[:coordinates][1] || '0'          
+          tab_hash[:tabLabel]   = tab.label || 'Signature 1'
+          tab_hash[:xPosition]  = tab.coordinates[0] || '0'
+          tab_hash[:yPosition]  = tab.coordinates[1] || '0'          
         end
 
         tab_hash[:documentId] = document_id || '0'
-        tab_hash[:scaleValue] = tab[:scaleValue] || 1
-
-        tab_hash[:name]       = tab[:name] if tab[:name]
-        tab_hash[:tabLabel]   = tab[:label] || 'Signature 1'
-        tab_hash[:width]      = tab[:width] if tab[:width]
-        tab_hash[:height]     = tab[:height] if tab[:width]
-        tab_hash[:value]      = tab[:value] if tab[:value]
-
+        tab_hash[:pageNumber] = tab.page_number
         tab_hash
       end
   end
