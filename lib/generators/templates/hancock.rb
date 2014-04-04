4e6f81bb95a27507251dc458b41228759ff4b0cf
kbokhonko <kolya.bokhonko@gmail.com>
2014-04-04 14:25:27 +0300

    add email template to configuration, refactor, action status

--- lib/generators/templates/hancock.rb
+++ lib/generators/templates/hancock.rb
@@ -1,20 +1,20 @@
 require 'hancock'
 
-Hancock.configure do |c|
-   c.username       = 'awesome@whereever.com'
-   c.password       = '12345'
-   c.integrator_key = 'YAY-9ED8E711-C191-4265-AFCC-253F6241207A'
-   c.account_id     = '999999'
-   c.endpoint       = 'https://www.docusign.net/restapi'
-   c.api_version    = 'v2'
+Hancock.configure do |config|
+  config.username       = 'gmhawash@gmail.com'
+  config.password       = 'WhatIs25*25'
+  config.integrator_key = 'RFXX-5e8080a0-88d3-4c41-94b6-d01674c2c4d8'
+  config.account_id     = '443799'
+  config.endpoint       = 'https://demo.docusign.net/restapi'
+  config.api_version    = 'v2'
 
   # c.event_notification = {
   #   :logging_enabled => true,
   #   :uri => 'http://callback.com',
   #   :include_document => true,
   # }
-  # c.email_template = {
-  #   :subject => 'sign me',
-  #   :blurb => 'whatever '
-  # }
+  config.email_template = {
+    :subject => 'subject from configuration',
+    :blurb => 'blurb from configuration '
+  }
 end
