c1d10b7ada5d19bfa36aefa09d2cbeb49fee003e
kbokhonko <kolya.bokhonko@gmail.com>
2014-04-08 16:40:29 +0300

    recipients, documents actions, tests

--- spec/libs/envelope_spec.rb
+++ spec/libs/envelope_spec.rb
@@ -3,26 +3,23 @@ require_relative '../spec_helper'
 describe Hancock::Envelope do
   include_context "configs"
   include_context "variables"
-
-  it "should send envelope with status 'created'" do
+  
+  before do
     envelope.add_document(document)
     envelope.add_signature_request(recipient, document, [tab])
+  end
 
+  it "should send envelope with status 'created'" do
     envelope.save
     envelope.status.should == "created" 
   end
 
   it "should send envelope with status 'sent'" do
-    envelope.add_document(document)
-    envelope.add_signature_request(recipient, document, [tab])
-
     envelope.send!
     envelope.status.should == "sent" 
   end
 
   it "should find envelope with status id" do
-    envelope.add_document(document)
-    envelope.add_signature_request(recipient, document, [tab])
     envelope.save
 
     en = Hancock::Envelope.find(envelope.identifier)
@@ -47,6 +44,23 @@ describe Hancock::Envelope do
 
     envelope1.save
     envelope1.status.should == "created"
+    envelope1.identifier.should_not == nil
+  end
+
+  it "action 'documents' should return correct result" do
+    envelope.save
+    documents = envelope.documents
+
+    documents.length.should == 1
+    documents.first.name.should == "test"
+  end
+
+  it "action 'recipients' should return correct result" do
+    envelope.save
+    recipients = envelope.recipients
+
+    recipients.length.should == 1
+    recipients.first.name.should == "Owner"
   end
 
 end
