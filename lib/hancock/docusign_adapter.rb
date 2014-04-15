55e4b6b4230c81208e389806511224416a092bd8
kbokhonko <kolya.bokhonko@gmail.com>
2014-04-15 17:31:35 +0300

    add comments to class DocusignAdapter

--- lib/hancock/docusign_adapter.rb
+++ lib/hancock/docusign_adapter.rb
@@ -5,18 +5,30 @@ module Hancock
       @envelope_id = envelope_id
     end
 
+    #
+    # The response returns the current envelope status
+    #
     def envelope
       JSON.parse(send_get_request("/accounts/#{Hancock.account_id}/envelopes/#{envelope_id}").body)
     end
 
+    #
+    # This retrieves the specified document from the envelope
+    #
     def documents
       JSON.parse(send_get_request("/accounts/#{Hancock.account_id}/envelopes/#{envelope_id}/documents").body)["envelopeDocuments"]
     end
 
+    #
+    # This returns a list of documents associated with the specified envelope
+    #
     def document(document_id)
       send_get_request("/accounts/#{Hancock.account_id}/envelopes/#{envelope_id}/documents/#{document_id}").body
     end
 
+    #
+    # This returns a list of recipients associated with the specified envelope
+    #
     def recipients
       JSON.parse(send_get_request("/accounts/#{Hancock.account_id}/envelopes/#{envelope_id}/recipients").body)
     end
