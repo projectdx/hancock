6cc9212dd0b59de25e7263782a50a19819b0da56
Andrew Yasinishyn <1@MacBook-Pro-1.local>
2014-04-15 18:59:38 +0300

    Recipient.reload!, Document.reload! methods tests

--- spec/libs/recipient_spec.rb
+++ spec/libs/recipient_spec.rb
@@ -3,6 +3,11 @@ require_relative '../spec_helper'
 describe Hancock::Recipient do
   include_context "configs"
   include_context "variables"
+  
+  before do
+    envelope.add_document(document)
+    envelope.add_signature_request({ recipient: recipient, document: document, tabs: [tab] })
+  end
 
   describe 'Defaults' do
     it 'Should generate identifier if not given' do 
@@ -24,6 +29,15 @@ describe Hancock::Recipient do
         routing_order:   1
        }
        Hancock::Recipient.new(params).recipient_type.should_not be(nil)
+    end
+
+    it 'Should reload recipients' do
+      envelope.save
+      recipients = envelope.recipients
+      envelope.add_signature_request({ recipient: recipient2, document: document, tabs: [tab] })
+      envelope.save
+
+      recipients.size.should_not eq( Hancock::Recipient.reload!(envelope).size )
     end
   end
   
