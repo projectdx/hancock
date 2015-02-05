e79f6b74c18a89be5929564f17939332d8f4f20e
Chuck Lauer Vose <vosechu@gmail.com>
2015-02-05 13:50:21 -0800

    II-6418: Add DocusignRecipient persistence class

---
+++ lib/hancock/recipient/docusign_recipient.rb
@@ -0,0 +1,68 @@
+module Hancock
+  class Recipient < Hancock::Base
+    class DocusignRecipient
+      attr_reader :recipient
+
+      extend Forwardable
+      def_delegators :@recipient, :envelope_identifier, :identifier
+
+      def initialize(recipient)
+        fail 'recipient requires an envelope_identifier' unless recipient.envelope_identifier
+        fail 'recipient requires an identifier' unless recipient.identifier
+
+        @recipient = recipient
+      end
+
+      def self.all_for(envelope_identifier)
+        Hancock::Request.send_get_request(
+          "/envelopes/#{envelope_identifier}/recipients")
+      end
+
+      def self.find(envelope_identifier, identifier)
+        Hancock::Request.send_get_request(
+          "/envelopes/#{envelope_identifier}/recipients/#{identifier}")
+      end
+
+      def tabs
+        Hancock::Request.send_get_request(
+          "/envelopes/#{envelope_identifier}/recipients/#{identifier}/tabs")
+      end
+
+      def create_tabs_from_json(tabs)
+        Hancock::Request.send_post_request(
+          "/envelopes/#{envelope_identifier}/recipients/#{identifier}/tabs",
+          tabs
+          )
+      end
+
+      def delete
+        json_body = { :signers => [{ :recipient_id => identifier }] }.to_json
+
+        Hancock::Request.send_delete_request(
+          "/envelopes/#{envelope_identifier}/recipients",
+          json_body
+          )
+      end
+
+      def create
+        Hancock::Request.send_post_request(
+          "/envelopes/#{envelope_identifier}/recipients",
+          { :signers => [to_hash] }.to_json
+          )
+      end
+
+      private
+
+      def to_hash
+        {
+          :clientUserId  => recipient.client_user_id,
+          :email         => recipient.email,
+          :idCheck       => recipient.id_check,
+          :name          => recipient.name,
+          :recipientId   => recipient.identifier,
+          :routingOrder  => recipient.routing_order
+        }
+      end
+    end
+  end
+end
---
+++ spec/lib/recipient/docusign_recipient_spec.rb
@@ -0,0 +1,92 @@
+describe Hancock::Recipient::DocusignRecipient do
+  let(:recipient) {
+    Hancock::Recipient.new(
+      :envelope_identifier => 'fiji',
+      :identifier => 'snorkel-puffer')
+  }
+  subject { described_class.new(recipient) }
+
+  describe '.all_for' do
+    it 'makes a request' do
+      expect(Hancock::Request).to receive(:send_get_request)
+        .with('/envelopes/yosemite-sam/recipients')
+
+      described_class.all_for('yosemite-sam')
+    end
+  end
+
+  describe '.find' do
+    it 'makes a request' do
+      expect(Hancock::Request).to receive(:send_get_request)
+        .with('/envelopes/yosemite-sam/recipients/bugs-bunny')
+
+      described_class.find('yosemite-sam', 'bugs-bunny')
+    end
+  end
+
+  describe '#tabs' do
+    it 'makes a request' do
+      expect(Hancock::Request).to receive(:send_get_request)
+        .with('/envelopes/fiji/recipients/snorkel-puffer/tabs')
+
+      subject.tabs
+    end
+  end
+
+  describe '#create_tabs_from_json' do
+    it 'makes a request' do
+      json = '{ "some": "thingy" }'
+
+      expect(Hancock::Request).to receive(:send_post_request)
+        .with('/envelopes/fiji/recipients/snorkel-puffer/tabs', json)
+
+      subject.create_tabs_from_json(json)
+    end
+  end
+
+  describe '#delete' do
+    it 'makes a request' do
+      json = { :signers => [{ :recipient_id => 'snorkel-puffer' }] }.to_json
+
+      expect(Hancock::Request).to receive(:send_delete_request)
+        .with('/envelopes/fiji/recipients', json)
+
+      subject.delete
+    end
+  end
+
+  describe '#create' do
+    let(:recipient) {
+      Hancock::Recipient.new(
+        :client_user_id => '1',
+        :email => 'jimmy@example.com',
+        :name => 'Jimmy Stewart',
+        :recipient_type => 'signer',
+        :identifier => '42',
+        :id_check => true,
+        :envelope_identifier => 'barbra-streisand'
+      )
+    }
+
+    it 'makes a request' do
+      json = {
+        :signers => [
+          {
+            :clientUserId => '1',
+            :email => 'jimmy@example.com',
+            :idCheck => true,
+            :name => 'Jimmy Stewart',
+            :recipientId => '42',
+            :routingOrder => 1
+          }
+        ]
+      }.to_json
+
+      expect(Hancock::Request).to receive(:send_post_request)
+        .with('/envelopes/barbra-streisand/recipients', json)
+
+      subject.create
+    end
+  end
+end
+
