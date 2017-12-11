b96b0e0327018d235cac26e34e16d1ec68690767
pfriedman <psfriedman@gmail.com>
2017-12-11 13:18:27 -0800

    add shared on anchored tab

--- lib/hancock/anchored_tab.rb
+++ lib/hancock/anchored_tab.rb
@@ -13,18 +13,19 @@ module Hancock
 
     def to_h
       {
-        :anchorString             => anchor_text,
-        :anchorXOffset            => coordinates[0],
-        :anchorYOffset            => coordinates[1],
-        :pageNumber               => page_number,
-        :anchorIgnoreIfNotPresent => true,
-        :validationPattern        => validation_pattern,
-        :validationMessage        => validation_message,
-        :width                    => width,
-        :fontSize                 => docusign_font_size(font_size),
-        :optional                 => optional,
-        :tabLabel                 => label,
-        :required                 => required
+        anchorString:             anchor_text,
+        anchorXOffset:            coordinates[0],
+        anchorYOffset:            coordinates[1],
+        pageNumber:               page_number,
+        anchorIgnoreIfNotPresent: true,
+        validationPattern:        validation_pattern,
+        validationMessage:        validation_message,
+        width:                    width,
+        fontSize:                 docusign_font_size(font_size),
+        optional:                 optional,
+        tabLabel:                 label,
+        required:                 required,
+        shared:                   shared
       }.reject{ |_, value| value.nil? }
     end
   end
